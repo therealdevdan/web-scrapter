429d995b62cb31a4b59ca28f162d9b064b4a8150
Alex <alexanderdanilchenko888@gmail.com>
2025-06-27 02:58:39 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 use trpl::{Either, Html}; 
-        // enum Either<A, B> {
-        //     Left(A),
-        //     Right(B),
-        // }
 use std::env;
 
 async fn page_title(url: &str) -> (&str, Option<String>) {
@@ -17,16 +13,13 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
 fn main() {
     let args: Vec<String> = env::args().collect();
  
-    // функция ..trpl::run.. как бы создает среду, которая управляет 
-    // асинхронным кодом (main так делать не умеет, поскольку 
-    // она является точкой входа в программу)
     trpl::run(async {
-        let res_page_title_1 = page_title(&args[1]); // Здесь возвращён ленивый фьючерс, который без использования ничего 
-        let res_page_title_2 = page_title(&args[2]); // делать не будет,  а если его использовать, то он вернёт кортеж 
-//                                                                                                                                  ( &str, Option<String> )
+        let res_page_title_1 = page_title(&args[1]);  
+        let res_page_title_2 = page_title(&args[2]);  
+                                                                                                                                  ( &str, Option<String> )
         let (url, maybe_title) = 
-            match trpl::race(res_page_title_1, res_page_title_2).await { // trpl::race, которая возвращает значение, указывающее,
-                Either::Left(left) => left,             // какой из переданных ей фьючерсов завершится первым.                            
+            match trpl::race(res_page_title_1, res_page_title_2).await {
+                Either::Left(left) => left,                                        
                 Either::Right(right) => right,          
         };                                                                                                                 
 
